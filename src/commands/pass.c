19e7eff935ae1133bc6886e47c8f43e8c524f58a
Clemon <r.goulmot@hotmail.fr>
2018-04-24 15:44:14 +0200

    debug pas

--- src/commands/pass.c
+++ src/commands/pass.c
@@ -9,13 +9,11 @@
 
 void	pass(client_t *client, const char *arg)
 {
-	if (!arg)
-		write(client->fd, "530 Permission denied\n", 22);
-	else if (!client->account.user)
+	if (!client->account.user)
 		write(client->fd, "503 Log in USER first\n", 22);
 	else if (client->account.user
 	&& strcmp(client->account.user, USER) == 0
-	&& (PASS == "" || strcmp(arg, PASS) == 0)){
+	&& (PASS == "" || (arg && strcmp(arg, PASS) == 0))){
 		client->account.logged = 1;
 		write(client->fd, "230 Login successful\n", 21);
 	}
